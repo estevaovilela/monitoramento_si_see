9c2eb544d3b91821cda30ab9eb5145dd1031ae4a
estevaovilela <estevao.avilela@gmail.com>
2018-12-14 18:07:58 -0200

    Add map to shiny frist trials. need improvement

--- shiny.R
+++ shiny.R
@@ -6,67 +6,118 @@ library(rsconnect)
 library(DT)
 library(data.table)
 library(lubridate)
+library(leaflet)
+library(sp)
 
 theme_set(theme_bw())
 
-# Reading -----------------------------------------------------------------
 
-bd_criacao <- data.table::fread(input = "criacao.csv",
-                          sep = ",",
-                          header = TRUE,
-                          stringsAsFactors = FALSE,
-                          encoding = "UTF-8")
+# Reading - SQL -----------------------------------------------------------
 
-bd_encerramento <- data.table::fread(input = "encerramento.csv",
-                                sep = ",",
-                                header = TRUE,
-                                stringsAsFactors = FALSE,
-                                encoding = "UTF-8")
+#Insira suas credenciais para obter acesso ao Banco NGI:
+con <- dbConnect(RMariaDB::MariaDB(),
+                 dbname = "db_ngi",
+                 host = "10.23.185.10",
+                 port = 3306,
+                 user = "arthur_cheib",
+                 password = rstudioapi::askForPassword("Database password"))
 
-bd_matricula <- data.table::fread(input = "matricula.csv",
-                                sep = ",",
-                                header = TRUE,
-                                stringsAsFactors = FALSE,
-                                encoding = "UTF-8")
+#Listagem de todas as tabelas que existem no banco NGI:
+tabelas_banco <- dbListTables(con)
 
+bd_criacao <- RMariaDB::dbReadTable(con, tabelas_banco[1])
+bd_encerramento <- RMariaDB::dbReadTable(con, tabelas_banco[2])
+bd_matricula <- RMariaDB::dbReadTable(con, tabelas_banco[3])
+
+
+# Reading - Geo - SRE -----------------------------------------------------
+
+sre_geo <- rgdal::readOGR(dsn = path.expand("C:/Users/m7531296/OneDrive/Estevao/Profissional/SEE/projects/localizacao-escolas/shp/Contorno SREs.shp"),
+                          layer = "Contorno SREs")
+
+sre_geo@data$SRE <- str_to_upper(sre_geo@data$Nome.SRE)
+sre_geo@data$SRE[c(7,12,15,24,26:28,34)] <- c("SAO JOAO DEL REI", "CONSELHEIRO LAFAIETE", "SAO SEBASTIAO DO PARAISO",
+                                            "CORONEL FABRICIANO", "METROPOLITANA A", "METROPOLITANA B",
+                                            "METROPOLITANA C", "GOVERNADOR VALADARES")
+
+# Reading - csv-----------------------------------------------------------------
+
+#bd_criacao <- data.table::fread(input = "criacao.csv",
+#                          sep = ",",
+#                          header = TRUE,
+#                          stringsAsFactors = FALSE,
+#                          encoding = "UTF-8")
+
+#bd_encerramento <- data.table::fread(input = "encerramento.csv",
+#                                sep = ",",
+#                                header = TRUE,
+#                                stringsAsFactors = FALSE,
+#                                encoding = "UTF-8")
+
+#bd_matricula <- data.table::fread(input = "matricula.csv",
+#                                sep = ",",
+#                                header = TRUE,
+#                                stringsAsFactors = FALSE,
+#                                encoding = "UTF-8")
 
 # Wrangling ---------------------------------------------------------------
 
 bd_criacao <- bd_criacao %>% 
-  mutate(DIA = dmy("12-12-2018")) %>% 
-  group_by(SRE, DIA) %>% 
+  group_by(SRE, DATA) %>% 
   summarise(TOTAL_TURMA_PA = sum(QT_TURMA_PA, na.rm = TRUE),
-            TOTAL_ALUNO_PA = sum(QT_ALUNO_PA, na.rm = TRUE),
             TOTAL_TURMA_CRIADA = sum(QT_TURMA_CRIADA, na.rm = TRUE),
-            TOTAL_TURMA_AUTORIZADA = sum(QT_TURMA_AUTORIZADA, na.rm = TRUE),
-            TOTAL_ALUNO_ENTURMADO = sum(QT_ALUNO_ENTURMADO, na.rm = TRUE)) %>% 
-  #mutate(TX_ENTURMACAO = TOTAL_ALUNO_ENTURMADO / TOTAL_ALUNO_PA,
-  #       TX_CRIACAO = TOTAL_TURMA_CRIADA / TOTAL_TURMA_PA,
-  #       TX_AUTORIZACAO = TOTAL_TURMA_AUTORIZADA / TOTAL_TURMA_PA) %>% 
+            TOTAL_TURMA_AUTORIZADA = sum(QT_TURMA_AUTORIZADA, na.rm = TRUE)) %>% 
   mutate(TX_CRIACAO = TOTAL_TURMA_CRIADA / TOTAL_TURMA_PA,
          TX_AUTORIZACAO = TOTAL_TURMA_AUTORIZADA / TOTAL_TURMA_PA) %>% 
   select(-starts_with("TOTAL"))
 
 bd_encerramento <- bd_encerramento %>% 
-  mutate(DIA = dmy("12-12-2018")) %>% 
-  group_by(SRE, DIA) %>% 
+  group_by(SRE, DATA) %>% 
   summarise(TOTAL_ALUNO_ENTURMADO  = sum(QT_ALUNO_ENTURMADO_ATIVO, na.rm = TRUE),
             TOTAL_ALUNO_ENCERRADO = sum(QT_ALUNO_ENCERRADO, na.rm = TRUE)) %>% 
   mutate(PRIMEIRO = first(TOTAL_ALUNO_ENTURMADO)) %>% 
   mutate(TX_ENCERRAMENTO = TOTAL_ALUNO_ENCERRADO / PRIMEIRO) %>% 
-  select(-starts_with("TOTAL"))
+  select(-starts_with("TOTAL"), -PRIMEIRO)
 
 bd_matricula <- bd_matricula %>% 
-  mutate(DIA = dmy("12-12-2018")) %>% 
-  group_by(SRE, DIA) %>% 
+  group_by(SRE, DATA) %>% 
   summarise(TOTAL_ALUNO_MATRICULADO  = sum(QT_ALUNO_MATRICULADO, na.rm = TRUE),
             TOTAL_ALUNO_ENTURMADO = sum(QT_ALUNO_ENTURMADO, na.rm = TRUE)) %>% 
   mutate(TX_ENTURMACAO = TOTAL_ALUNO_ENTURMADO / TOTAL_ALUNO_MATRICULADO) %>% 
   select(-starts_with("TOTAL"))
 
-min_date <- min(bd_criacao$DIA)
-max_date <- max(bd_criacao$DIA)
+min_date <- min(bd_criacao$DATA)
+max_date <- max(bd_criacao$DATA)
 
+# Tabela agrupada
+base_tabela <- bd_criacao %>% 
+  group_by(SRE) %>% 
+  top_n(1, wt = DATA) %>%
+  left_join(bd_encerramento, by = c("SRE", "DATA")) %>% 
+  left_join(bd_matricula, by = c("SRE", "DATA")) %>% 
+  arrange(TX_ENCERRAMENTO)
+
+# Leaflet -----------------------------------------------------------------
+
+sre_geo@data <- sre_geo@data %>% 
+  left_join(base_tabela, by = c("SRE"))
+
+pal <- colorNumeric(palette = "Blues",
+                    domain = sre_geo@data$TX_ENCERRAMENTO)
+
+content <- paste(sep = "<br>",
+                str_to_title(sre_geo@data$SRE),
+                paste(as.character(round(sre_geo@data$TX_ENCERRAMENTO*100, digits = 2)), "%"))
+
+leaflet::leaflet(sre_geo) %>% 
+  leaflet::addProviderTiles(providers$OpenStreetMap) %>% 
+  leaflet::addPolygons(data = sre_geo, # LAD polygon data from geojson
+                       fillColor = ~pal(TX_ENCERRAMENTO),
+                       fillOpacity = 1,
+                       weight = 1,  # line thickness
+                       opacity = 1,  # line transparency
+                       color = "black", # line colour
+                       popup = content) 
 # UI ----------------------------------------------------------------------
 
 ui <- fluidPage(
@@ -107,6 +158,26 @@ ui <- fluidPage(
                           )
                       )
              ),
+             tabPanel(title = "Mapa",
+                      sidebarLayout(
+                        sidebarPanel(
+                          # Instrucoes
+                          tags$p("Selecione qual processo deseja visualizar."),
+                          # Escolher Processo
+                          selectInput(inputId = "processo",
+                                      label = "Processo",
+                                      choices = c("Criacao de turmas" = "criacao",
+                                                  "Autorizacao de turmas" = "autorizacao",
+                                                  "Encerramento" = "encerramento",
+                                                  "Enturmacao" = "enturmacao"),
+                                      multiple = FALSE,
+                                      selected = "criacao")
+                        ),
+                        mainPanel(
+                          plotOutput("plot_map")
+                        )
+                      )
+             ),
              tabPanel(title = "Sobre",
                       tags$p("Dashboard desenvolvido para uso da",
                              " Subsecretaria de Informacoes e Tecnologias",
@@ -119,15 +190,6 @@ ui <- fluidPage(
 # SERVER ------------------------------------------------------------------
 
 server <- function(input, output) {
-  
-  # Tabela
-  base_tabela <- bd_criacao %>% 
-    group_by(SRE) %>% 
-    top_n(1, wt = DIA) %>%
-    left_join(bd_encerramento, by = c("SRE", "DIA")) %>% 
-    left_join(bd_matricula, by = c("SRE", "DIA")) %>% 
-    select(-PRIMEIRO) %>% 
-    arrange(TX_ENCERRAMENTO)
   
   output$table_sre <- DT::renderDT({
     datatable(base_tabela,
