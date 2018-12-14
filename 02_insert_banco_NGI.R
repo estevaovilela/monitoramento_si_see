2acececa0a00cec2772366b76a248818d2249da8
estevaovilela <estevao.avilela@gmail.com>
2018-12-14 11:32:40 -0200

    Add second file

---
+++ 02_insert_banco_NGI.R
@@ -0,0 +1,41 @@
+library(dbConnect)
+library(RMariaDB)
+
+#Insira suas credenciais para obter acesso ao Banco NGI:
+con <- dbConnect(RMariaDB::MariaDB(),
+                 dbname = "db_ngi",
+                 host = "10.23.185.10",
+                 port = 3306,
+                 user = "xxxx",
+                 password = "xxxx")
+
+#Listagem de todas as tabelas que existem no banco NGI:
+tabelas_banco <- dbListTables(con)
+
+# Vetor que contém os tipos das colunas das tabelas do banco na mesma ordem do vetor tabelas_banco:
+lista_variaveis_banco <- list(CRIACAO = c(rep("TEXT", 10), rep("DOUBLE", 5)),
+                             ENCERRAMENTO = c(rep("TEXT", 12), rep("DOUBLE", 3)),
+                              MATRICULA = c(rep("TEXT", 10), rep("DOUBLE", 2)))
+
+#Inicio da Função de Teste para checagem e inserção dos dados
+checkVars_insertData <- function(lista_bases, lista_variaveis_banco, tabelas_banco) {
+  
+  if ((sum(dbDataType(con, as.data.frame(lista_bases)) != lista_variaveis_banco)) != 0) {
+    
+  print("A quantidade de colunas da data.frame a ser inserida ou não é igual a do banco \n
+        ou são variáveis de tipos - class - diferentes as variáveis do banco")
+    
+  stop()
+    
+  } else {
+  
+  dbWriteTable(conn = con, tabelas_banco, lista_bases, append = TRUE, row.names = FALSE)
+  
+  }    
+    
+}
+
+# Rodar a função de inserção e checagem sobre as listas de data.frames
+pwalk(list(lista_data_frames, lista_variaveis_banco, tabelas_banco), checkVars_insertData)
+
+dbDisconnect(con)
